e313614b90168461ad63e374de3d499de223ff9e
echo <1540302560@qq.com>
2020-07-30 14:27:12 +0800

    update .gitignore

--- junior/helloworld.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-
-hello = tf.constant("hello world")
-print(hello)
-print(hello.numpy())
--- junior/tf2_base.py
+++
@@ -1,6 +0,0 @@
-"""
-tf2常用的参数或者是操作
-"""
-import tensorflow as tf
-
-print(tf.zeros_initializer())
